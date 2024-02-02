99afc706cbd35ee8e1b57b52d47236e5808ae317
arcenter <101260108+arcenter@users.noreply.github.com>
2024-02-02 16:32:58 +0400

    Cleanup

--- Pages/EditTaskPage.xaml.cs
+++ Pages/EditTaskPage.xaml.cs
@@ -17,7 +17,7 @@ namespace TemporaTasks.Pages
         readonly MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
         IndividualTask task;
 
-        public DateTimePicker dtpic;
+        public DateTimePicker datePickerUserControl;
 
         public EditTaskPage(IndividualTask task)
         {
@@ -25,22 +25,12 @@ namespace TemporaTasks.Pages
 
             this.task = task;
             TaskNameTextbox.Text = task.TaskName;
-            // DTPicker.Value = task.DueDT;
-            dateTextBox.Text = $"{task.DueDT.Value.Year}-{task.DueDT.Value.Month.ToString().PadLeft(2, '0')}-{task.DueDT.Value.Day.ToString().PadLeft(2, '0')}";
-
-            int hour = task.DueDT.Value.Hour;
-            string apm;
-            if (hour < 12)
+            
+            if (task.DueDT.HasValue)
             {
-                apm = "AM";
+                dateTextBox.Text = DTHelper.DateToString(task.DueDT.Value);
+                timeTextBox.Text = DTHelper.TimeToString(task.DueDT.Value);
             }
-            else
-            {
-                apm = "PM";
-                hour = hour - 12;
-            }
-
-            timeTextBox.Text = $"{hour.ToString().PadLeft(2, '0')}:{task.DueDT.Value.Minute.ToString().PadLeft(2, '0')} {apm}";
 
             TaskNameTextbox.Focus();
         }
@@ -57,98 +47,69 @@ namespace TemporaTasks.Pages
 
         private void Page_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter && !myPopUp.IsOpen)
+            if (e.Key == Key.Enter && !datePickerPopUp.IsOpen)
             {
                 ConfirmButton_MouseDown(null, null);
             }
             else if (e.Key == Key.Escape)
             {
-                mainWindow.FrameView.Navigate(new HomePage());
+                if (datePickerPopUp.IsOpen) datePickerPopUp.IsOpen = false;
+                else mainWindow.FrameView.GoBack();
             }
-        }
-
-        private void CancelButton_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            ((Border)sender).Background.BeginAnimation(Brush.OpacityProperty, new DoubleAnimation(((bool)e.NewValue) ? 1 : 0.5, TimeSpan.FromMilliseconds(250)));
-        }
-
-        private void CancelButton_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            mainWindow.FrameView.Navigate(new HomePage());
-        }
-
-        private void CancelButton_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point mousePosition = e.GetPosition(sender as UIElement);
-            ToolTip tooltip = (ToolTip)CancelButton.ToolTip;
-            tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
-            tooltip.HorizontalOffset = mousePosition.X;
-            tooltip.VerticalOffset = mousePosition.Y;
-        }
-
-        private void ConfirmButton_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            ((Border)sender).Background.BeginAnimation(Brush.OpacityProperty, new DoubleAnimation(((bool)e.NewValue)? 1 : 0.5, TimeSpan.FromMilliseconds(250)));
-        }
-
-        private void ConfirmButton_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Nullable<DateTime> newDueDate = null;
-            try
-            {
-                string[] splits = dateTextBox.Text.Split('-');
-                int year = int.Parse(splits[0]);
-                int month = int.Parse(splits[1]);
-                int day = int.Parse(splits[2]);
-
-                int hour, minute;
-                if (timeTextBox.Text.Length == 0)
-                {
-                    hour = 0;
-                    minute = 0;
-                }
-                else
-                {
-                    splits = timeTextBox.Text.Split(" ");
-                    bool am = (splits[1].ToLower() == "am");
-
-                    splits = splits[0].Split(":");
-                    hour = int.Parse(splits[0]) + (am ? 0 : 12);
-                    minute = int.Parse(splits[1]);
-                }
-
-                newDueDate = new(year, month, day, hour, minute, 0);
-            }
-            catch { return; }
-
-            task.TaskTimer.Stop();
-            TaskFile.TaskList[TaskFile.TaskList.IndexOf(task)] = new IndividualTask(TaskNameTextbox.Text, task.CreatedDT, newDueDate, null); // DTPicker.Value
-            TaskFile.SaveData();
-            mainWindow.FrameView.RemoveBackEntry();
-            mainWindow.FrameView.Navigate(new HomePage());
-        }
-
-        private void ConfirmButton_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point mousePosition = e.GetPosition(sender as UIElement);
-            ToolTip tooltip = (ToolTip)ConfirmButton.ToolTip;
-            tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
-            tooltip.HorizontalOffset = mousePosition.X;
-            tooltip.VerticalOffset = mousePosition.Y;
         }
 
         private void Border_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            ((Border)sender).Background.BeginAnimation(Brush.OpacityProperty, new DoubleAnimation((bool)e.NewValue? 0.75 : 0.25, TimeSpan.FromMilliseconds(250)));
+            ((Border)sender).Background.BeginAnimation(Brush.OpacityProperty, new DoubleAnimation((bool)e.NewValue ? 1 : 0.5, TimeSpan.FromMilliseconds(250)));
         }
 
-        private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Border_MouseMove(object sender, MouseEventArgs e)
         {
-            dtpic = new DateTimePicker(task.DueDT);
-            dtpic.textBox = dateTextBox;
-            dtpic.popUp = myPopUp;
-            myPopUp.Child = dtpic;
-            myPopUp.IsOpen = true;
+            Point mousePosition = e.GetPosition(sender as UIElement);
+            ToolTip tooltip = (ToolTip)((Border)sender).ToolTip;
+            tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
+            tooltip.HorizontalOffset = mousePosition.X;
+            tooltip.VerticalOffset = mousePosition.Y;
+        }
+
+        private void calendar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            datePickerUserControl = new DateTimePicker(task.DueDT);
+            datePickerUserControl.textBox = dateTextBox;
+            datePickerUserControl.popUp = datePickerPopUp;
+            datePickerPopUp.Child = datePickerUserControl;
+            datePickerPopUp.IsOpen = true;
+        }
+
+        private void CancelButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mainWindow.FrameView.GoBack();
+        }
+
+        private void ConfirmButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DateBorder.BorderThickness = TimeBorder.BorderThickness = new Thickness(0);
+            Nullable<DateTime> newDueDate;
+            try
+            {
+                newDueDate = DTHelper.StringToDateTime(dateTextBox.Text, timeTextBox.Text);
+            }
+            catch (IncorrectDateException)
+            {
+                DateBorder.BorderThickness = new Thickness(2);
+                return;
+            }
+            catch (IncorrectTimeException)
+            {
+                TimeBorder.BorderThickness = new Thickness(2);
+                return;
+            }
+
+            task.TaskTimer.Stop();
+            TaskFile.TaskList[TaskFile.TaskList.IndexOf(task)] = new IndividualTask(TaskNameTextbox.Text, task.CreatedDT, newDueDate, null);
+            TaskFile.SaveData();
+            mainWindow.FrameView.RemoveBackEntry();
+            mainWindow.FrameView.Navigate(new HomePage());
         }
     }
 }
